ecf9dea96eb24dcb72882e4896c5082d90dbd6b9
trof <reatrof@gmail.com>
2017-04-15 22:14:46 +0300

    posts model updated

--- models/PostModel.js
+++ models/PostModel.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose');
 
 const PostScheme = new mongoose.Schema({
-    title: {
-        type: String
-    },
-    content: {
-        type: String
-    },
-    password: {
-        type: String,
-        required: [true, 'field password is required']
-    }
+    title: { type: String },
+    content: { type: String },
+    description: {type: String},
+    date: { type: String },
+    image: {type: String},
+    tags: { type: Array, required: [true, 'field password is required'] },
+    likes: { type: Number },
+    watches: {type: String},
+    comments: { type: Number },
+    translation: { type: Boolean }
 });
 
-module.exports = mongoose.model('Post', PostScheme);
+PostScheme.methods.getDecription = (content) => {
+    return content.split(' ').splice(0, 40).join(' ') + '...';
+};
+
+Post = mongoose.model('Post', PostScheme);
+
+// let post = new Post;
+// post.title = 'Моделирование данных в MongoDB';
+// post.content = "Наверное, многим интересно, как же команде Telegram удалось сделать популярную для мессенджеров функцию голосовых звонков уже сразу после запуска разительно отличающейся по качеству в лучшую сторону перед многими другими VoIP — сервисами. Во время изучения работы Telegram Calls я обратил внимание на интересную техническую деталь, которая используется на сетевом уровне и помогает уменьшить задержки при прохождении пакетов, когда при соединении используются релейные сервера. Если взглянуть на дамп трафика, который проходит при звонке, в котором использовался релейный сервер, можно заметить, что трафик определяется протоколом RIP. IP на 91. — это сервер Telegram. Я снимал дамп трафика на своём VPN — сервере, поэтому адрес клиента из локальной подсети.";
+// post.date = new Date('DD.MM.YYYY');
+// post.image = 'post1.jpg';
+// post.tags = ['Javascript, Python, NodeJS'];
+// post.likes = 12;
+// post.watches = 35;
+// post.comments = 3;
+// post.translation = true;
+//
+// post.save();
+
+module.exports = Post;
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const router = express.Router();
+const Posts = require('../models/PostModel');
 
 // const User = require('../models/UserModel');
 
 router.get('/', (req, res, next) => {
     let options = {
-        title: 'this is main page'
+        title: 'this is main page',
+        posts: {}
     };
-    res.render('index', {options: options})
+    Posts.find({}).then((posts) => {
+        options = {
+            posts: posts.map((post) => {
+                return {
+                    title: post.title,
+                    content: post.content,
+                    description: post.getDecription(post.content),
+                    date: post.date,
+                    image: post.image,
+                    tags: post.tags,
+                    likes: post.likes,
+                    watches: post.watches,
+                    comments: post.comments,
+                    translation: post.translation
+                }
+            })
+        };
+        res.render('index', {options: options});
+    }).catch(next);
+
 });
 
 module.exports = router;
